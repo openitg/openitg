6044a79902ca2e44c5591aa747899ee6ce12de8e
Patrick McIlroy <itgpmc@gmail.com>
2008-02-24 12:13:01 +0000

    git-svn-id: https://openitg.svn.sourceforge.net/svnroot/openitg@50 83fadc84-e282-4d84-a09a-c4228d6ae7e5

--- src/MiscITG.h
+++ src/MiscITG.h
@@ -1,9 +1,14 @@
 #ifndef MISC_ITG_H
 #define MISC_ITG_H
+#include "StdString.h"
+
 int GetNumCrashLogs();
 int GetNumMachineEdits();
 int GetIP();
 int GetRevision();
 int GetNumMachineScores();
+CString GetSerialNumber();
+
+static CString g_SerialNum;
 #endif
 

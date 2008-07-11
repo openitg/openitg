e09d3dc0796f1eade6bfac3c8a0e52b443ea15c0
Patrick McIlroy <itgpmc@gmail.com>
2008-07-11 04:38:33 +0000

    git-svn-id: https://openitg.svn.sourceforge.net/svnroot/openitg@432 83fadc84-e282-4d84-a09a-c4228d6ae7e5

--- start-openitg.bat
+++ start-openitg.bat
@@ -4,4 +4,4 @@ rem if a new patch directory exists, then move it
 if exist Data\new-patch move Data\new-patch Data\patch
 
 rem Start OpenITG
-Program\OpenITG.exe --type=Preferences-cabinet
+Program\OpenITG-PC.exe --type=Preferences-cabinet

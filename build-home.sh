cd5f0725273621258fd8b5818dec70c304e9508f
Carl Myers <cmyers@cmyers.org>
2011-02-28 23:58:58 -0800

    Some build-related fixes, dep update
    * Noted that autoconf tools are needed, v1.9 or older
    * Fixed build scripts to take a number to call `make -jX`

--- build-home.sh
+++ build-home.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+# XXX: Move this to make land!
+./generate-version
 
 ./autogen.sh
-# TODO: make this actually build 64-bit
-./configure --with-x --with-gnu-ld --target=i386-pc-linux-gnu --host=i386-pc-linux-gnu
-make
+#./configure --with-x --with-gnu-ld --target=i386-pc-linux-gnu --host=i386-pc-linux-gnu
+./configure --with-x --with-gnu-ld
+if [ "x$1" == "x" ];
+then
+	make
+else
+	make -j$1
+fi
 
 

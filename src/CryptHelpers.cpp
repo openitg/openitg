36834e442e47af8f479364b36ba0e8edddba1d98
Toni Spets <toni.spets@iki.fi>
2011-03-14 19:25:15 +0200

    Revert "Link against system Crypto++ if available"
    This reverts commit df0fd44dd9cc4f5719e917d141856b4d076112c0.
    
    There is more to it than what I did, arcade signing broke down.

--- src/CryptHelpers.cpp
+++ src/CryptHelpers.cpp
@@ -6,21 +6,12 @@
 
 // crypt headers
 #include <typeinfo>
-#if defined(HAVE_LIBCRYPTOPP)
-	#include <crypto++/files.h>
-	#include <crypto++/filters.h>
-	#include <crypto++/cryptlib.h>
-	#include <crypto++/sha.h>
-	#include <crypto++/rsa.h>
-	#include <crypto++/osrng.h>
-#else
-	#include "crypto561/files.h"
-	#include "crypto561/filters.h"
-	#include "crypto561/cryptlib.h"
-	#include "crypto561/sha.h"
-	#include "crypto561/rsa.h"
-	#include "crypto561/osrng.h"
-#endif
+#include "crypto561/files.h"
+#include "crypto561/filters.h"
+#include "crypto561/cryptlib.h"
+#include "crypto561/sha.h"
+#include "crypto561/rsa.h"
+#include "crypto561/osrng.h"
 #if defined(_MSC_VER) && !defined(_XBOX) && _MSC_VER <= 1310
 #if defined(_DEBUG)
 #pragma comment(lib, "crypto561/cryptlib_vs2003/Win32/output/debug/cryptlib_vs2003.lib")
@@ -61,24 +52,7 @@ private:
 	bool m_waiting;
 };
 
-template <class T>
-class RageSourceTemplate : public Source
-{
-public:
-	RageSourceTemplate<T>(BufferedTransformation *attachment, T _store)
-		: Source(attachment), m_store(_store) {}
-        size_t Pump2(lword &byteCount, bool blocking=true)
-                {return m_store.TransferTo2(*AttachedTransformation(), byteCount, DEFAULT_CHANNEL, blocking);}
-        size_t PumpMessages2(unsigned int &messageCount, bool blocking=true)
-                {return m_store.TransferMessagesTo2(*AttachedTransformation(), messageCount, DEFAULT_CHANNEL, blocking);}
-        bool SourceExhausted() const
-                {return !m_store.AnyRetrievable() && !m_store.AnyMessages();}
-
-protected:
-        T m_store;
-};
-
-class RageFileSource : public RageSourceTemplate<RageFileStore>
+class RageFileSource : public SourceTemplate<RageFileStore>
 {
 public:
 	typedef FileStore::Err Err;
@@ -86,7 +60,7 @@ public:
 	typedef FileStore::ReadErr ReadErr;
 
 	RageFileSource( RageFileBasic *pFile, bool pumpAll, BufferedTransformation *attachment = NULL )
-		: RageSourceTemplate<RageFileStore>(attachment,RageFileStore(pFile)) {SourceInitialize(pumpAll, MakeParameters("InputBinaryMode", true, false));}
+		: SourceTemplate<RageFileStore>(attachment,RageFileStore(pFile)) {SourceInitialize(pumpAll, MakeParameters("InputBinaryMode", true, false));}
 };
 
 RageFileStore::ReadErr::ReadErr( const RageFileBasic &f ):

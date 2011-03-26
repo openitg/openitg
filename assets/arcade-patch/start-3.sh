073ea1e687d7c85bef4057d97f5a711e0851b4ad
Mark Cannon <vyhdycokio@gmail.com>
2011-03-26 16:13:42 -0400

    minor fixups in start-3.sh, updated patch.xml a bit

--- assets/arcade-patch/start-3.sh
+++ assets/arcade-patch/start-3.sh
@@ -3,20 +3,19 @@ if [ -f /itgdata/K ]; then
 	export __GL_FSAA_MODE=1
 fi
 
+# If we're on a kit and /itgdata/V exists, force the resolution to 640x480
+# using our prefs type. This retains ITGIO's global offset and other settings.
 if [ -f /itgdata/K ]; then
-	PARAM="--type=Preferences-kit"
-
-	# Use 640x480 resolution for the kit if /itgdata V exists.
 	if [ -f /itgdata/V ]; then
 		PARAM="--type=Preferences-kit-vga"
+	else
+		PARAM="--type=Preferences-kit"
 	fi
 elif [ -f /itgdata/S ]; then
 	PARAM="--type=Preferences-school"
 else
 	PARAM="--type=Preferences-cabinet"
 fi
-
-echo $PARAM
 
 if [ ! -f /stats/premium ]; then
 	echo >> /stats/StepMania.ini
@@ -32,11 +31,9 @@ export LD_LIBRARY_PATH=/stats/patch/lib
 
 # /stats/patch/install-permanent-patch.sh
 
-if [ -f /tmp/no-start-game ]
-then
+if [ -f /tmp/no-start-game ]; then
 	sleep 999d
+else
+	cd /itgdata
+	exec /stats/patch/openitg $PARAM
 fi
-
-cd /itgdata
-
-exec /stats/patch/openitg $PARAM

aaf59b2d7232d78f274142eaa530e28bfb30248e
Carl Myers <cmyers@cmyers.org>
2011-09-24 22:26:06 -0700

    build-home.sh now correctly invokes make without -j
    CR: infamouspat

--- build-home.sh
+++ build-home.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 ./autogen.sh
 #./configure --with-x --with-gnu-ld --target=i386-pc-linux-gnu --host=i386-pc-linux-gnu
 ./configure --with-x --with-gnu-ld
 if [ "x$1" == "x" ];
 then
-	make
+    echo "Running make"
+    make
 else
-	make -j$1
+    echo "Running make -j$1"
+    make -j$1
 fi
 
 

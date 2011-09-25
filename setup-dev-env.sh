d36b29ab69de1abf4229e10b5f299216e10d1591
Carl Myers <cmyers@cmyers.org>
2011-09-24 23:40:05 -0700

    setup-dev-env.sh helps devs setup an environment to run home version

---
+++ setup-dev-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RUN_DIR=run
+
+mkdir -p $RUN_DIR
+
+for i in `cd assets/d4 && ls`; do
+    cp -rl assets/d4/$i $RUN_DIR/$i;
+done
+
+for i in `cd assets/game-data && ls`; do
+    cp -rl assets/game-data/$i $RUN_DIR/$i;
+done
+
+ln -s ../src/openitg $RUN_DIR/openitg
+ln -s ../src/GtkModule.so $RUN_DIR/GtkModule.so

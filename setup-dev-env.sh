c1baa4d0a2acf98ac60533b7468147310867d9d7
Pat McIlroy <itgpmc@gmail.com>
2011-10-02 23:51:51 -0700

    add patch data and fix bug in Data dir creation in setup-dev-env.sh

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -9,8 +9,11 @@ for i in `cd assets/d4 && ls`; do
 done
 
 for i in `cd assets/game-data && ls`; do
-    cp -rl assets/game-data/$i $RUN_DIR/$i;
+    cp -rlf assets/game-data/$i $RUN_DIR;
 done
 
 ln -s ../src/openitg $RUN_DIR/openitg
 ln -s ../src/GtkModule.so $RUN_DIR/GtkModule.so
+
+mkdir $RUN_DIR/Data/patch
+ln -s ../../../assets/patch-data $RUN_DIR/Data/patch/patch

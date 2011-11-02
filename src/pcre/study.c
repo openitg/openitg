ea4d6a40161c2f9980513942987f1f2bbcbd1600
Mikko Rasa <tdb@tdb.fi>
2011-11-02 14:01:22 +0200

    Mark dummy as unused since it's apparently needed on some platforms (OpenITG on OS/2?  Oh yeah.)

--- src/pcre/study.c
+++ src/pcre/study.c
@@ -38,6 +38,11 @@ the external pcre header. */
 
 #include "internal.h"
 
+#ifdef __GNUC__
+#define UNUSED __attribute__((unused))
+#else
+#define UNUSED
+#endif
 
 
 /*************************************************
@@ -96,7 +101,7 @@ code. Apparently IBM isn't going to fix the problem, and we would rather not
 disable optimization (in this module it actually makes a big difference, and
 the pcre module can use all the optimization it can get). */
 
-volatile int dummy;
+volatile int dummy UNUSED;
 
 do
   {

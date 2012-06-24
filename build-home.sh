817ee5366d803b2fa103a029b16fa1c59ed5e59a
infamouspat <itgpmc@gmail.com>
2012-06-24 16:17:51 -0700

    How has this been in our repository for this long?:
    * Fix wrong use of test syntax in build-home.sh that resulted in make
      creating an unlimited amount of threads when the number of processes
      is not specified

--- build-home.sh
+++ build-home.sh
@@ -3,7 +3,7 @@
 ./autogen.sh
 #./configure --with-x --with-gnu-ld --target=i386-pc-linux-gnu --host=i386-pc-linux-gnu
 ./configure --with-x --with-gnu-ld
-if [ "x$1" == "x" ];
+if [ "x$1" = "x" ];
 then
     echo "Running make"
     make

d79cb9d443f009fe60441b7ca5fd15517e2ec5bb
oakwhiz <oakwhiz@minousoft.com>
2012-12-12 19:41:10 -0800

    Added script to install dependencies - easier build process

---
+++ getdependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install git-core make gcc g++ autoconf2.13 automake1.9 gettext libgl1-mesa-dev libglu1-mesa-dev libpng12-dev libjpeg62-dev liblua5.1-0-dev libvorbis-dev libmad0-dev libusb-dev libxrandr-dev libavcodec-dev libavformat-dev libswscale-dev libasound2-dev libresample1-dev libcrypto++-dev libgtk2.0-dev libsdl1.2-dev

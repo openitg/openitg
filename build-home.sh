92157599174a1e895fbb05ebe296c25822115ad2
Pat Mac <itgpmc@gmail.com>
2016-05-08 00:51:18 -0700

    remove hardcoded i386 target from build-home

--- build-home.sh
+++ build-home.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ./autogen.sh
-./configure --with-x --with-gnu-ld --target=i386-pc-linux-gnu --host=i386-pc-linux-gnu
-#./configure --with-x --with-gnu-ld
+./configure --with-x --with-gnu-ld
 if [ "x$1" = "x" ];
 then
     echo "Running make"

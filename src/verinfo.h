c1bbda1b60082d90866d588e3b928ceb74d61708
style_of_2001 <style_of_2001@tempemail.net>
2017-04-17 07:07:56 -0500

    fix lua for different targets, remove vdi files from tracking

--- src/verinfo.h
+++ src/verinfo.h
@@ -2,4 +2,4 @@
 
 #define BUILD_VERSION	"stupid-placeholder-tag-goddammit-github DEV"
 #define BUILD_DATE		"2017-04-14"
-#define BUILD_REVISION_TAG	"beta3-378-gc6bbea4"
+#define BUILD_REVISION_TAG	"beta3-379-gc613cc8"
